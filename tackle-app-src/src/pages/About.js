7caabe162ce2252518f6e049bc60f2b7585983a3
Luke Withrow <lukiewithrow@gmail.com>
2024-10-18 11:40:34 -0400

    fixed broken ReactJS formatting

--- tackle-app-src/src/pages/About.js
+++ tackle-app-src/src/pages/About.js
@@ -1,6 +1,8 @@
 export default function About(){
     return(
-        <h1>About Page</h1>
-		<p>I'm testing to see if commiting is working</p>
-    );
+		<>
+			<h1>About Page</h1>
+			<p>I'm testing to see if commiting is working</p>
+		</>
+	);
 }
