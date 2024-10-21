05b2efc65090667f2718235905366324a62a3cb9
Luke Hare <lph00003@mix.wvu.edu>
2024-10-21 11:46:54 -0400

    Updated WeatherConditions. Now maps days.

--- tackle-app-src/src/components/WeatherConditions.js
+++ tackle-app-src/src/components/WeatherConditions.js
@@ -1,5 +1,17 @@
-export default function WeatherConditions(){
+//Variables
+let $dayDivs = [];   //array containing all days of the week divs
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function click(){
+    console.log("clicked");
+}
+
+function WeatherConditions(){
     return(
-        <div>WeatherConditions Component</div>
+        <div>
+            {days.map((day, index) => ( <li onClick={click} key={index}>{day}</li>))}
+        </div>
     );
 }
+
+export default WeatherConditions
