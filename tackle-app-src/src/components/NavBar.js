15dc4a674ec89c11130668251eaf6ea9f8a3b6f1
Spencer Vida <swv00001@mix.wvu.edu>
2024-10-23 13:27:28 -0400

    Added Icons for navbar and favicon, assets folder to store favicon

--- tackle-app-src/src/components/NavBar.js
+++ tackle-app-src/src/components/NavBar.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 export default function NavBar(){
     return(
         <div id="navbutton-grid">
-			<Link to="/">Home</Link>
-			<Link to="/Information">Information</Link>
-			<Link to="/Personal">Personal</Link>
-			<Link to="/About">About</Link>
+		<Link to="/"><span class="material-icons">home</span>Home</Link>
+			<Link to="/Information"><span class="material-icons">info</span>Information</Link>
+			<Link to="/Personal"><span class="material-icons">account_circle</span>Personal</Link>
+			<Link to="/About"><span class="material-icons">groups</span>About</Link>
 		</div>
     );
 }
