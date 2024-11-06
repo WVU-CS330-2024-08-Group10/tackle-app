22cf7872b0e39ff36b53b7e9c74f9ce1426cb69a
Luke Withrow <lukiewithrow@gmail.com>
2024-11-06 13:33:03 -0500

    added fish swap buttons

--- tackle-app-src/src/pages/Personal.js
+++ tackle-app-src/src/pages/Personal.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
 
+const genericFish = {
+    species: {
+        name: "Species"
+    },
+    nickname: "Name",
+    timeCaught: new Date(0),
+    bodyCaught: "Location",
+    weight: 0,
+    length: 0,
+    sex: 0,
+    tackled: "Bait"
+}
 const genericProfile = {
     id: 1,
     username: "JeremyWade_Official",
@@ -18,21 +30,62 @@ const genericProfile = {
             length: 22, // inches?
             sex: -1,
             tackled: "Very Good Bait"
-        }
+        },
+        {
+            species: {
+                name: "Catfinch"
+            },
+            nickname: "Little John",
+            timeCaught: new Date(),
+            bodyCaught: "Kanawha River, WV", // REPLACE WITH some sort of location object later
+            weight: 51, // lbs?
+            length: 22, // inches?
+            sex: -1,
+            tackled: "Very Good Bait"
+        },
+        {
+            species: {
+                name: "A trout"
+            },
+            nickname: "slipper",
+            timeCaught: new Date(),
+            bodyCaught: "Coal River, WV", // REPLACE WITH some sort of location object later
+            weight: 51, // lbs?
+            length: 22, // inches?
+            sex: -1,
+            tackled: "Very Good Bait"
+        },
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
+        genericFish,
     ]
 };
 
 export default function Personal() {
     const [username, setUsername] = useState(genericProfile.username);
-
-    let fishListInit = [];
-    for (let i = 0; i < genericProfile.fishList.length; i++) {
-        let fish = genericProfile.fishList[i];
-        fishListInit.push(<FishListElement fish={fish} key={`fishlist-${i}`}/>);
-    }
-    const [fishList, setFishList] = useState(fishListInit);
+    const [fishList, setFishList] = useState(genericProfile.fishList);
     const [nickname, setNickname] = useState(genericProfile.nickname);
     const [gender, setGender] = useState(genericProfile.gender);
+
+    function swapFish(index1, index2) {
+        if (index1 < 0 || index2 < 0 || index1 > fishList.length - 1 || index2 > fishList.length - 1) return;
+        let fishListTemp = fishList;
+        
+        let fishTemp = fishListTemp[index1];
+        fishListTemp[index1] = fishListTemp[index2];
+        fishListTemp[index2] = fishTemp;
+
+        // for some reason, you have to make a new array
+        setFishList([...fishListTemp]);
+    }
 
     return(
         <div id="profile"> 
@@ -44,10 +97,23 @@ export default function Personal() {
             
             <div id="profile-right">
                 <h1>Welcome back, {username}!</h1>
-                <h3>Fish List:</h3>
+                <h3>Fish List ({fishList.length}):</h3>
 
                 <div id="profile-fishlist">
-                    {fishList}
+                    {fishList.map((fish, i) => (<div id="profile-fish" key={`fish-${i}`}>
+                        <div className="profile-fish-info">
+                            <div className="profile-fish-content">{fish.nickname}</div> 
+                            <div className="profile-fish-seperator">--</div> 
+                            <div className="profile-fish-content">{fish.species.name}</div> 
+                            <div className="profile-fish-seperator">--</div> 
+                            <div className="profile-fish-content">{fish.bodyCaught}</div> 
+                            <div className="profile-fish-seperator">--</div> 
+                            <div className="profile-fish-content">{fish.timeCaught.toLocaleString('en-US', {month: 'numeric', day: 'numeric', year: 'numeric'})}</div>
+                        </div>
+                        <button onClick={() => swapFish(i, i - 1)}>^</button>
+                        <button onClick={() => swapFish(i, i + 1)}>v</button>
+                        <button>Edit</button>
+                    </div>))}
                 </div>
             </div>
             
@@ -55,12 +121,3 @@ export default function Personal() {
     );
 }
 
-function FishListElement(args) {
-    let nickname = args.fish.nickname;
-    let speciesName = args.fish.species.name;
-    let bodyCaught = args.fish.bodyCaught;
-    let timeCaught = args.fish.timeCaught;
-    let timeCaughtString = timeCaught.toLocaleString('en-US', {month: 'numeric', day: 'numeric', year: 'numeric'});
-
-    return <p>{nickname} -- {speciesName} -- {bodyCaught} -- {timeCaughtString}</p>;
-}
