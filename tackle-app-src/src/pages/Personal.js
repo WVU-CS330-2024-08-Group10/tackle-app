f3c7fe5d057fc43ca71514627f5946dba5f57fb7
Luke Withrow <lukiewithrow@gmail.com>
2024-11-12 21:48:39 -0500

    added all fishform functionality

--- tackle-app-src/src/pages/Personal.js
+++ tackle-app-src/src/pages/Personal.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import ReactModal from 'react-modal';
 
-const genericFish = {
+const emptyFish = {
     species: {
-        name: "Species"
+        name: ""
     },
-    nickname: "Name",
+    nickname: "",
     timeCaught: new Date(0),
-    bodyCaught: "Location",
-    weight: 0,
-    length: 0,
-    sex: 0,
-    tackled: "Bait"
+    bodyCaught: "",
+    weight: null,
+    length: null,
+    sex: -1,
+    tackled: ""
 }
 const genericProfile = {
     id: 1,
@@ -19,7 +19,7 @@ const genericProfile = {
     nickname: "Jeremy Wade",
     gender: "male",
     favSpots: [],
-    fishList: [
+    fishlist: [
         {
             species: {
                 name: "Catfish"
@@ -55,66 +55,98 @@ const genericProfile = {
             length: 22, // inches?
             sex: -1,
             tackled: "Very Good Bait"
-        },
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
-        genericFish,
+        }
     ]
 };
 
 export default function Personal() {
-    const [username, setUsername] = useState(genericProfile.username);
-    const [fishlist, setFishlist] = useState(genericProfile.fishList);
-    const [nickname, setNickname] = useState(genericProfile.nickname);
-    const [gender, setGender] = useState(genericProfile.gender);
-    const [fishEditing, setFishEditing] = useState(-1); 
-    const [renderFishform, setRenderFishform] = useState(false);
-    const [isFishformEditing, setIsFishformEditing] = useState(false);
+    ReactModal.setAppElement('body');
+
+    const [profile, setProfile] = useState(genericProfile);
+
+    const [fishIndex, setFishIndex] = useState(-1); // index in fishlist of fish being edited
+    const [renderFishform, setRenderFishform] = useState(false); 
+    const [isFishformEditing, setIsFishformEditing] = useState(false); // indicates whether a fish is being edited (true) or added (false)
+    const [fishEdit, setFishEdit] = useState(emptyFish); // fish in fishform being edited
 
     function swapFish(index1, index2) {
-        if (index1 < 0 || index2 < 0 || index1 > fishlist.length - 1 || index2 > fishlist.length - 1) return;
-        let fishlistTemp = fishlist;
+        if (index1 < 0 || index2 < 0 || index1 > profile.fishlist.length - 1 || index2 > profile.fishlist.length - 1) return;
+        let fishlistTemp = [...profile.fishlist];
         
         let fishTemp = fishlistTemp[index1];
         fishlistTemp[index1] = fishlistTemp[index2];
         fishlistTemp[index2] = fishTemp;
 
-        // for some reason, you have to make a new array
-        setFishlist([...fishlistTemp]);
+        setProfile({...profile, fishlist: fishlistTemp});
     }
 
-    function openFish(index, editing = true) {
+    function openFish(index) {
+        setIsFishformEditing(true);
+        setFishIndex(index);
+
+        setFishEdit(profile.fishlist[index]);
         setRenderFishform(true);
-        if (editing != isFishformEditing) setIsFishformEditing(editing);
-        setFishEditing(index);
     }
 
-    function submitTheFish() {
+    function addFish() {
+        setIsFishformEditing(false);
+        setFishIndex(profile.fishlist.length);
+
+        let fishTemp = {...emptyFish};
+        fishTemp.timeCaught = new Date();
+
+        setFishEdit(fishTemp);
+        setRenderFishform(true);
+    }
+
+    function submitFish() {
+        setRenderFishform(false);
+
+        let fishTemp = {...fishEdit};
+        if (fishTemp.nickname === "") fishTemp.nickname = `Fish #${fishIndex + 1}`;
+        if (fishTemp.species.name === "") fishTemp.species = {name: "Catfish"};
+        if (fishTemp.bodyCaught === "") fishTemp.bodyCaught = "Kanawha River, WV";
+
+        let fishlistTemp = [...profile.fishlist];
+        fishlistTemp[fishIndex] = fishTemp;
+        setProfile({...profile, fishlist: fishlistTemp});
+    }
+
+    function cancelFish() {
         setRenderFishform(false);
     }
+
+    function removeFish() {
+        setRenderFishform(false);
+
+        let fishlistTemp = [...profile.fishlist];
+        fishlistTemp.splice(fishIndex, 1);
+        console.log(fishlistTemp);
+
+        setProfile({...profile, fishlist: fishlistTemp});
+    }
+
+    // Format date object to the appropriate string for the value of an HTML local-datetime input 
+    function dateToLocalDatetimeString(date) {
+        return new Date(date.getTime() + new Date().getTimezoneOffset() * -60 * 1000).toISOString().substring(0, 16);
+    }
+
 
     return(
         <div id="profile"> 
             <div id="profile-left">
                 <img id="profile-picture" src={require('../assets/jeremyPfp.jpg')} alt="Your profile picture"/>
-                <p><b>Nickname:</b> {nickname}</p>
-                <p><b>Gender:</b> {gender}</p>
+                <p><b>Username:</b> {profile.username}</p>
+                <p><b>Nickname:</b> {profile.nickname}</p>
+                <p><b>Gender:</b> {profile.gender}</p>
             </div>
             
             <div id="profile-right">
-                <h1>Welcome back, {username}!</h1>
-                <h3>Fish List ({fishlist.length}):</h3>
+                <h1>Welcome back, {profile.nickname}!</h1>
+                <h3>Fish List ({profile.fishlist.length}):</h3>
 
                 <div id="profile-fishlist">
-                    {fishlist.map((fish, i) => (<div id="profile-fish" key={`fish-${i}`}>
+                    {profile.fishlist.map((fish, i) => (<div id="profile-fish" key={`fish-${i}`}>
                         <div className="profile-fish-info">
                             <div className="profile-fish-content">{fish.nickname}</div> 
                             <div className="profile-fish-seperator">--</div> 
@@ -122,68 +154,79 @@ export default function Personal() {
                             <div className="profile-fish-seperator">--</div> 
                             <div className="profile-fish-content">{fish.bodyCaught}</div> 
                             <div className="profile-fish-seperator">--</div> 
-                            <div className="profile-fish-content">{fish.timeCaught.toLocaleString('en-US', {month: 'numeric', day: 'numeric', year: 'numeric'})}</div>
+                            <div className="profile-fish-content">{fish.timeCaught.toLocaleString('en-US', {month: 'numeric', day: 'numeric', year: 'numeric', hour: '2-digit', minute:'2-digit'})}</div>
                         </div>
                         <button onClick={() => swapFish(i, i - 1)}>^</button>
                         <button onClick={() => swapFish(i, i + 1)}>v</button>
                         <button onClick={() => openFish(i)}>Edit</button>
                     </div>))}
+                    {profile.fishlist.length < 1 ? <p>It's... empty. Something fishy is going on here</p>: null}
                 </div>
+
+                <button onClick={addFish}>Add Fish</button>
             </div>
             
             <ReactModal isOpen={renderFishform}>
                 <form id="fishform">
-                    <h1>{isFishformEditing ? `Editing \"${fishlist[fishEditing].nickname}\"` : "Congrats On Your New Catch!"}</h1>
+                    <h1>{isFishformEditing ? `Editing \"${profile.fishlist[fishIndex] !== undefined ? profile.fishlist[fishIndex].nickname : "Null"}\"` : "Congrats On Your New Catch!"}</h1>
                     <div>
                         <p>
                             <label htmlFor="nickname">Nickname: </label>
-                            <input id="nickname" name="nickname" placeholder="Big John" defaultValue={(typeof fishlist[fishEditing] !== 'undefined') ? fishlist[fishEditing].nickname : ""}/>
+                            <input id="nickname" name="nickname" placeholder={`Fish #${fishIndex + 1}`} value={fishEdit.nickname} onChange={(e) => setFishEdit({...fishEdit, nickname: e.target.value}) } />
+                        </p>
+                        <p>
+                            <label htmlFor="species">Species: </label>
+                            <input id="species" name="species" placeholder="Catfish" value={fishEdit.species.name} onChange={(e) => setFishEdit({...fishEdit, species: {name: e.target.value}}) } />
                         </p>
                     </div>
 
                     <div>
                         <p>
                             <label htmlFor="timeCaught">Time Caught: </label>
-                            <input type="datetime-local" id="timeCaught" name="timeCaught" defaultValue={(typeof fishlist[fishEditing] !== 'undefined') ? fishlist[fishEditing].timeCaught : ""}/>
+                            <input type="datetime-local" id="timeCaught" name="timeCaught" value={dateToLocalDatetimeString(fishEdit.timeCaught)} onChange={(e) => setFishEdit({...fishEdit, timeCaught: new Date(e.target.value)}) }/>
                         </p>
 
                         <p>
                             <label htmlFor="bodyCaught">Body Caught: </label>
-                            <input id="bodyCaught" name="bodyCaught" defaultValue={(typeof fishlist[fishEditing] !== 'undefined') ? fishlist[fishEditing].bodyCaught : ""}/>
+                            <input id="bodyCaught" name="bodyCaught" placeholder="Kanawha River, WV" value={fishEdit.bodyCaught} onChange={(e) => setFishEdit({...fishEdit, bodyCaught: e.target.value}) }/>
                         </p>
                     </div>
 
                     <div>
                         <p>
                             <label htmlFor="weight">Weight (lbs): </label>
-                            <input id="weight" name="weight" defaultValue={(typeof fishlist[fishEditing] !== 'undefined') ? fishlist[fishEditing].weight : ""}/>
+                            <input id="weight" name="weight" value={(fishEdit.weight === null) ? "" : fishEdit.weight} onChange={(e) => setFishEdit({...fishEdit, weight: e.target.value}) }/>
                         </p>
 
                         <p>
                             <label htmlFor="length">Length (in): </label>
-                            <input id="length" name="length" defaultValue={(typeof fishlist[fishEditing] !== 'undefined') ? fishlist[fishEditing].length : ""}/>
+                            <input id="length" name="length" value={(fishEdit.length === null) ? "" : fishEdit.length} onChange={(e) => setFishEdit({...fishEdit, length: e.target.value}) }/>
                         </p>
 
                         <p>
                             <label htmlFor="tackled">Tackled: </label>
-                            <input id="tackled" name="tackled" defaultValue={(typeof fishlist[fishEditing] !== 'undefined') ? fishlist[fishEditing].tackled : ""}/>
-                        </p>
-
-                        <p>
-                            <div id="fishform-sex-label">Sex:</div>
-                            <div id="fishform-sex-radio">
-                                <input type="radio" id="male" name="sex" value="male" />
-                                <label htmlFor="male">Male</label><br />
-                                <input type="radio" id="female" name="sex" value="female" />
-                                <label htmlFor="female">Female</label><br />
-                                <input type="radio" id="indeterminate" name="sex" value="indeterminate" />
-                                <label htmlFor="indeterminate">Indeterminate</label>
-                            </div>
+                            <input id="tackled" name="tackled" placeholder="Very Good Bait" value={fishEdit.tackled} onChange={(e) => setFishEdit({...fishEdit, tackled: e.target.value}) }/>
                         </p>
                     </div>
-                </form>
+                    
+                    <div>
+                        <div id="fishform-sex-label">Sex:</div>
+                        <div id="fishform-sex-radio">
+                            <input type="radio" id="male" name="sex" value="0" checked={fishEdit.sex === 0} onChange={(e) => setFishEdit({...fishEdit, sex: parseInt(e.target.value)}) }/>
+                            <label htmlFor="male">Male</label><br />
+                            <input type="radio" id="female" name="sex" value="1" checked={fishEdit.sex === 1} onChange={(e) => setFishEdit({...fishEdit, sex: parseInt(e.target.value)}) }/>
+                            <label htmlFor="female">Female</label><br />
+                            <input type="radio" id="indeterminate" name="sex" value="-1" checked={fishEdit.sex === -1} onChange={(e) => setFishEdit({...fishEdit, sex: parseInt(e.target.value)}) }/>
+                            <label htmlFor="indeterminate">Indeterminate</label>
+                        </div>
+                    </div>
 
-                <button onClick={submitTheFish}>Submit :3 XP</button>
+                    <p>
+                        <button onClick={submitFish}>Submit :3 XP</button>
+                        <button onClick={cancelFish}>Cancel</button>
+                        {isFishformEditing ? <button onClick={removeFish}>REMOVE this fish</button> : null}
+                    </p>
+                </form>
             </ ReactModal>
 
         </div>
