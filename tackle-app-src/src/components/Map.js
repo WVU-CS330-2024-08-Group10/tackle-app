20bb1803b8e84efece166d7433f89e5e07bee5fb
Christian Williams <christian.williams777@gmail.com>
2024-11-18 10:02:54 -0500

    Overhauled map to be solely in the component file (Map.js) as well as tweaking it's CSS to size the map down for future use

--- tackle-app-src/src/components/Map.js
+++ tackle-app-src/src/components/Map.js
@@ -1,7 +1,95 @@
-function Map(){
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+ import * as LEsri from 'esri-leaflet';
+
+const Map = () => {
+    useEffect(() => {
+        const map = L.map('map').setView([38.8976, -80.4549], 7);
+        var fishBox = document.getElementById("fish");
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var fishingLayer = LEsri.dynamicMapLayer({
+            url: 'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer',
+            opacity: 0.7
+        }).addTo(map);
+
+        const nonLakeUrls = [
+            'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer/4',
+            'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer/5',
+            'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer/13',
+            'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer/14'
+        ];
+
+        const lakeUrls = [
+            'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer/7',
+            'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer/8',
+            'https://services.wvgis.wvu.edu/arcgis/rest/services/Applications/dnrRec_fishing/MapServer/9'
+        ];
+
+        const combinedLayer = L.featureGroup();
+
+        lakeUrls.forEach(url => {
+        LEsri.featureLayer({
+            url: url,
+            onEachFeature: (feature, layer) => {
+                layer.on("click", function() {
+                    setFish(feature.properties, fishBox, "Lake");
+                });
+            },
+            opacity: 0.0
+        }).addTo(combinedLayer);
+        });
+
+        nonLakeUrls.forEach(url => {
+        LEsri.featureLayer({
+            url: url,
+            onEachFeature: (feature, layer) => {
+                layer.on("click", function() {
+                    setFish(feature.properties, fishBox, "NotLake");
+                });
+            },
+            opacity: 0.0
+        }).addTo(combinedLayer);
+        });
+
+        combinedLayer.addTo(map);
+
+        return () => {
+            // Clean up map layers when component unmounts
+            map.remove();
+          };
+    }, []);
+
     return(
-        <div id="map"></div>
+        <div id="mapBody">
+            <div id="map"></div>
+            <div id="fish">FISH</div>
+        </div>
     );
+}
+
+function setFish(properties, fishBox, urlType) {
+    let fishTypes = ["ChanCatFish", "Crappie", "StripBass", "LrgmthBass", "Musky", "WhtBass", "Walleye"];
+    var fishString = "";
+
+    if(urlType == "Lake") {
+        fishString = properties.LakeName;
+    } else {
+        fishString = properties.Name;
+    }
+
+    fishTypes.forEach(fish => {
+        if(properties[fish] == 1) {
+            fishString += ("\n" + fish);
+        }
+    });
+
+    fishBox.innerHTML = fishString;
 }
 
 export default Map;
--- tackle-app-src/src/pages/Home.js
+++ tackle-app-src/src/pages/Home.js
@@ -3,7 +3,7 @@ import Map from "../components/Map";
 export default function Home(){
     return(
         <>
-            <h1 className="page-header">Home Page</h1>
+            <h1 className="page-header"></h1>
             <Map /> 
         </>
     );
